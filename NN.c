5d6310c790820bd16eca6ca0ae4e62c30d2b5c39
Le Quang Phuc <phuclequang2006@gmail.com>
2024-11-30 20:43:03 +0700

    Add multi-class classification

--- NN.c
+++ NN.c
@@ -4,7 +4,6 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <limits.h>
 
 #define db double
 #define ll long long
@@ -14,18 +13,20 @@
 
 
 // Model configuration
-ll epochs = 200;
+ll epochs = 1000;
 ll batch_size = 2;
-db lr = 0.1;
+db lr = 0.01;
 ll nodes_per_layer[] = {0, 4, 4, 1};
-char *activation_per_layer[] = {"#", "relu", "relu", "sigmoid"};
+char *activation_per_layer[] = {"#", "relu", "relu", "softmax"};
+char *loss_function = "categorical_crossentropy";
 
 
 // Model parameters (weight, bias, etc.)
-ll m, n;
-db in[1005][1005], out[1005], batch_in[1005][1005], batch_out[1005];
-db W[1005][1005], B[1005][1005], Y[1005][1005], A[1005][1005];
-db dW[1005][1005], dB[1005][1005], dA[1005][1005];
+ll m, n, num_of_classes = 0;
+db in[5005][5005], out[5005], batch_in[5005][5005], batch_out[5005];
+db W[5005][5005], B[5005][5005], Y[5005][5005], A[5005][5005];
+db dW[5005][5005], dB[5005][5005], dA[5005][5005];
+ll class[5005];
 
 
 // Activation function
@@ -37,14 +38,22 @@ db tanh(db x) {return (exp(x) - exp(-x)) / (exp(x) + exp(-x));}
 db tanh_dx(db x) {return 1 - tanh(x) * tanh(x);}
 db linear(db x) {return x;}
 db linear_dx(db x) {return 1;}
+db softmax(db x, ll layer, ll node) {
+    db total_exp = 0;
+    for(ll cnode=0;cnode<nodes_per_layer[layer];cnode++) {
+        total_exp += exp(Y[layer][cnode]);
+    }
+    return exp(x) / total_exp;
+}
 
-db activation_fn(db x, ll layer) {
+db activation_fn(db x, ll layer, ll node) {
     if(activation_per_layer[layer] == "relu") return relu(x);
     else if(activation_per_layer[layer] == "sigmoid") return sigmoid(x);
     else if(activation_per_layer[layer] == "tanh") return tanh(x);
+    else if(activation_per_layer[layer] == "softmax") return softmax(x, layer, node);
     else return linear(x);
 }
-db activation_fn_dx(db x, ll layer) {
+db activation_fn_dx(db x, ll layer, ll node) {
     if(activation_per_layer[layer] == "relu") return relu_dx(x);
     else if(activation_per_layer[layer] == "sigmoid") return sigmoid_dx(x);
     else if(activation_per_layer[layer] == "tanh") return tanh_dx(x);
@@ -52,13 +61,81 @@ db activation_fn_dx(db x, ll layer) {
 }
 
 
-// Loss function and gradient
-db loss_fn(db truth, db predict) {
-    return -truth * log(predict) - (1 - truth) * log(1 - predict);
+// One-hot encoding
+int cmp(const void *a, const void *b) {
+    ll *x = (ll *) a, *y = (ll *) b;
+    return *x > *y;
+}
+void one_hot() {
+    for(ll i=0;i<m;i++) {
+        bool is_duplicate = 0;
+
+        for(ll j=0;j<num_of_classes;j++) {
+            if(out[i] == class[j]) {
+                is_duplicate = 1;
+                break;
+            }
+        }
+
+        if(!is_duplicate) class[num_of_classes++] = out[i];
+    }
+
+    qsort(class, num_of_classes, sizeof(class[0]), cmp);
+}
+int find_one_hot_pos(db x) {
+    for(ll i=0;i<num_of_classes;i++) {
+        if(class[i] == x) return i;
+    }
+    return 0;
 }
 
-db loss_fn_dx(db truth, db predict) {
-    return -truth / predict + (1 - truth) / (1 - predict);
+
+// Get position of max value
+ll argmax(ll layer) {
+    db ma = A[layer][0]; ll pos = 0;
+    for(ll i=0;i<nodes_per_layer[layer];i++) {
+        if(A[layer][i] > ma) {ma = A[layer][i]; pos = i;}
+    }
+    return pos;
+}
+
+
+// Get position of min value
+ll argmin(ll layer) {
+    db mi = A[layer][0]; ll pos = 0;
+    for(ll i=0;i<nodes_per_layer[layer];i++) {
+        if(A[layer][i] < mi) {mi = A[layer][i]; pos = i;}
+    }
+    return pos;
+}
+
+
+// Loss function and gradient
+db loss_fn(db truth, db predict) {
+    if(loss_function == "binary_crossentropy") {
+        return -(truth) * log(predict) - (1 - truth) * log(1 - predict);
+    }
+    else if(loss_function == "categorical_crossentropy") {
+        return -log(A[sz(nodes_per_layer) - 1][find_one_hot_pos(truth)]);
+    }
+    else if(loss_function == "mse") {
+        return pow(truth - predict, 2);
+    }
+    else return 0;
+}
+void loss_fn_dx(db truth, db predict) {
+    if(loss_function == "binary_crossentropy") {
+        dA[sz(nodes_per_layer)-1][0] = -truth / predict + (1 - truth) / (1 - predict);
+    }
+    else if(loss_function == "categorical_crossentropy") {
+        for(ll node=0;node<num_of_classes;node++) {
+            dA[sz(nodes_per_layer)-1][node] = A[sz(nodes_per_layer)-1][node] - (node == find_one_hot_pos(truth));
+        }
+    }
+    else if(loss_function == "mse") {
+        dA[sz(nodes_per_layer)-1][0] = 2 * (predict - truth);
+    }
+    else return;
 }
 
 
@@ -118,36 +195,43 @@ ll create_batch() {
 // Forward propagation
 void forward() {
     for(ll layer=1;layer<sz(nodes_per_layer);layer++) {
-        // printf("######## Layer %lld ########\n", layer);
         ll w_order = 0;
         for(ll node=0;node<nodes_per_layer[layer];node++) {
-            // printf("--- Node %lld ---\n", node);
-            // printf("w: ");
-
             for(ll pre_node=0;pre_node<nodes_per_layer[layer-1];pre_node++) {
                 Y[layer][node] += W[layer][w_order] * A[layer-1][pre_node];
-
-                // printf("%lf ", W[layer][w_order]);
 
                 w_order++;
             }
 
             Y[layer][node] += B[layer][node];
-            A[layer][node] = activation_fn(Y[layer][node], layer);
-
-            // printf("\nb: %lf\ny: %lf\na: %lf\n", B[layer][node], Y[layer][node], A[layer][node]);
         }
+
+        for(ll node=0;node<nodes_per_layer[layer];node++) {
+            A[layer][node] = activation_fn(Y[layer][node], layer, node);
+        }
+
+        //Print result
+        // printf("######## Layer %lld ########\n", layer);
+        // w_order = 0;
+        // for(ll node=0;node<nodes_per_layer[layer];node++) {\
+        //     printf("--- Node %lld ---\n", node);
+        //     printf("w: ");
+        //     for(ll pre_node=0;pre_node<nodes_per_layer[layer-1];pre_node++) {
+        //         printf("%lf ", W[layer][w_order++]);
+        //     }
+
+        //     printf("\nb: %lf\ny: %lf\na: %lf\n", B[layer][node], Y[layer][node], A[layer][node]);
+        // }
         // printf("\n");
     }
 }
 
 
 // Backward propagation
-void backward(db dE) {
+void backward(db truth, db predict) {
     memset(dA, 0, sizeof(dA));
 
-    dA[sz(nodes_per_layer)-1][0] = dE;
-    // printf("%lf\n", dA[sz(nodes_per_layer)-1][0]);
+    loss_fn_dx(truth, predict);
 
     for(ll layer=sz(nodes_per_layer)-1;layer>=1;layer--) {
         // printf("######## Layer %lld ########\n", layer);
@@ -158,16 +242,16 @@ void backward(db dE) {
             // printf("dW: ");
 
             for(ll pre_node=0;pre_node<nodes_per_layer[layer-1];pre_node++) {
-                dW[layer][w_order] += dA[layer][node] * activation_fn_dx(Y[layer][node], layer) * A[layer-1][pre_node];
+                dW[layer][w_order] += dA[layer][node] * (activation_per_layer[layer] != "softmax" ? activation_fn_dx(Y[layer][node], layer, node) : 1) * A[layer-1][pre_node];
 
-                dA[layer-1][pre_node] += dA[layer][node] * activation_fn_dx(Y[layer][node], layer) * W[layer][w_order];
+                dA[layer-1][pre_node] += dA[layer][node] * (activation_per_layer[layer] != "softmax" ? activation_fn_dx(Y[layer][node], layer, node) : 1) * W[layer][w_order];
 
                 // printf("%lf ", dW[layer][w_order]);
 
                 w_order++;
             }
 
-            dB[layer][node] += dA[layer][node] * activation_fn_dx(Y[layer][node], layer);
+            dB[layer][node] += dA[layer][node] * (activation_per_layer[layer] != "softmax" ? activation_fn_dx(Y[layer][node], layer, node) : 1);
 
             // printf("\ndB: %lf\ndA: %lf\n", dB[layer][node], dA[layer][node]);
         }
@@ -206,6 +290,11 @@ int main() {
         for(ll j=0;j<n;j++) scanf("%lf", &in[i][j]);
         scanf("%lf", &out[i]);
     }
+
+    if(loss_function == "categorical_crossentropy") {
+        one_hot();
+        nodes_per_layer[sz(nodes_per_layer) - 1] = num_of_classes;
+    }
     
     // Set input size
     nodes_per_layer[0] = n;
@@ -226,6 +315,7 @@ int main() {
         printf("+++++++++ EPOCH %lld +++++++++\n", epoch);
 
         ll curr_batch_size, batch_num = 0;
+        db total_loss = 0;
 
         // Load through dataset
         while((curr_batch_size = create_batch()) > 0) {
@@ -252,10 +342,13 @@ int main() {
 
                 // Backward propagation
                 // printf("********** Backward **********\n");
-                backward(loss_fn_dx(truth, A[sz(nodes_per_layer)-1][0]));
+                backward(truth, A[sz(nodes_per_layer)-1][0]);
             }
 
-            printf("Batch %lld - Loss: %.9lf\n", ++batch_num, loss / curr_batch_size);
+            // Calculate total loss per epoch and print
+            total_loss += loss / curr_batch_size;
+            batch_num++;
+            printf("Batch %lld - Loss: %.9lf\n", batch_num, total_loss / batch_num);
             fflush(stdout);
 
 
@@ -280,6 +373,10 @@ int main() {
         // Predict
         forward();
 
-        printf("Testcase #%lld - Predict value: %.9lf\n", i, A[sz(nodes_per_layer)-1][0]);
+        printf("Testcase #%lld - Predict value: ", i);
+        if(loss_function == "categorical_crossentropy") printf("%lld", class[argmax(sz(nodes_per_layer)-1)]);
+        else if(loss_function == "binary_crossentropy") printf("%lld", A[sz(nodes_per_layer)-1][0] > 0.5);
+        else printf("%lf", A[sz(nodes_per_layer)-1][0]);
+        printf("\n");
     }
 }
