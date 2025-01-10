d3d011b7aaf69224bb19f97b37988fa1ed67ef67
Le Quang Phuc <phuclequang2006@gmail.com>
2025-01-10 13:21:51 +0700

    Get MNIST from TF and add stuff to output

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,20 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
 from sklearn.model_selection import train_test_split
 from collections import Counter
 
-df = pd.read_csv("mnist_train.csv")
+# Load MNIST dataset
+(X, Y), (_, _) = mnist.load_data()
 
-Y = df['label'].to_numpy()
-
-T = df.drop('label', axis=1)
-
-X = T.to_numpy()
-X = X / 255.0
+# Flatten the images
+X = X.reshape(X.shape[0], -1).astype('float32') / 255.0
 
 # Perform balanced train-test split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=42)
+test_size = 0.2
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, stratify=Y, random_state=42)
 
 # Function to select a balanced subset
 def get_balanced_subset(X, Y, size):
@@ -42,5 +41,10 @@ TT_subset = np.concatenate((X_subset, Y_subset.reshape(len(Y_subset), 1)), axis=
 # Shuffle the array before saving with random_state = 42
 np.random.seed(42)
 np.random.shuffle(TT_subset)
-
 np.savetxt("MAIN.INP", TT_subset, fmt='%.14f')
+
+# Write the size of dataset, the dimension of the first element of X_subset and the train_size to the first line of MAIN.INP
+with open("MAIN.INP", "r+") as file:
+    content = file.read()
+    file.seek(0, 0)
+    file.write(f"{TT_subset.shape[0]} {X_subset.shape[1]} {1 - test_size}\n" + content)
